a0419752978e30d65dee6cb4b98e780814f15d25
Zach <zach.lee@brooklinen.com>
2025-05-01 17:23:29 -0400

    styling

--- pages/content/src/index.ts
+++ pages/content/src/index.ts
@@ -2,13 +2,18 @@ function checkCookie(cookieName) {
   return document.cookie.indexOf(cookieName + '=') > -1;
 }
 
+function getStringBetween(str, start, end) {
+  const result = str.match(new RegExp(start + '(.*)' + end));
+
+  return result[1];
+}
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   //let all cookies set in this function expire after 30 days
   let expires = '';
   const date = new Date();
   date.setTime(date.getTime() + 30 * 24 * 60 * 60 * 1000);
   expires = '; expires=' + date.toUTCString();
-
   if (request.context) {
     switch (true) {
       case request.context == 'overlayGrid':
@@ -26,7 +31,6 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         const previewBar = document
           .querySelector('#chrome-extension-boilerplate-react-vite-content-view-root')
           .shadowRoot.querySelector('#themePreviewBar');
-
         if (request.checked) {
           document.cookie = `_bl_chrome-extension_preview-bar=true; expires=${expires}; path=/;`;
           previewBar.style.display = 'initial';
@@ -43,9 +47,22 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     //refresh page after cookie is set
     location.reload();
   } else if (request.loading) {
+    const promoJSON = JSON.parse(
+      `{${getStringBetween(document.querySelector('#defaultData').textContent, 'window.Brooklinen.promo = {', '};')}}`,
+    );
     sendResponse({
       overlayGrid: checkCookie('_bl_chrome-extension_grid'),
       previewBar: checkCookie('_bl_chrome-extension_preview-bar'),
+      promoObject: promoJSON,
     });
+  } else if (request.clearCookies) {
+    const cookieNames = document.cookie.split(/=[^;]*(?:;\s*|$)/);
+    // Remove any that match the pattern
+    for (let i = 0; i < cookieNames.length; i++) {
+      if (/^_bl_dev__/.test(cookieNames[i])) {
+        document.cookie = cookieNames[i] + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/';
+      }
+    }
+    location.reload();
   }
 });
--- pages/popup/src/Popup.tsx
+++ pages/popup/src/Popup.tsx
@@ -1,12 +1,6 @@
 import '@src/Popup.css';
 import { withErrorBoundary, withSuspense } from '@extension/shared';
 
-const notificationOptions = {
-  type: 'basic',
-  title: 'Injecting content script error',
-  message: 'You cannot inject script here!',
-} as const;
-
 const Popup = () => {
   const openPage = page =>
     chrome.tabs.create({
@@ -18,11 +12,6 @@ const Popup = () => {
     e.preventDefault();
     console.log(e.target.checked, e.target.name, 'target');
     const [tab] = await chrome.tabs.query({ currentWindow: true, active: true });
-
-    console.log(tab);
-
-    sessionStorage.setItem(e.target.name, e.target.checked);
-    console.log(sessionStorage, 'sessionStorage');
     if (e.target.checked) e.target.checked = false;
     else e.target.checked = true;
     await chrome.tabs.sendMessage(tab.id, {
@@ -46,7 +35,11 @@ const Popup = () => {
   //use document cookies on the current tab to determine if toggles should be pre-checked
   const checkCookies = async () => {
     const [tab] = await chrome.tabs.query({ currentWindow: true, active: true });
+    console.log(tab);
 
+    chrome.windows.get(tab.windowId, function (win) {
+      console.log('window', win); // THIS IS THE WINDOW OBJECT
+    });
     await chrome.tabs
       .sendMessage(tab.id, {
         loading: true,
@@ -54,38 +47,45 @@ const Popup = () => {
       .then(res => {
         document.querySelector('[name="overlayGrid"]').checked = res.overlayGrid;
         document.querySelector('[name="previewBar"]').checked = res.previewBar;
+        //document.querySelector('#promoVersion').innerHTML = res.promoObject.version;
       });
   };
 
   checkCookies();
 
+  const clearDevCookies = async () => {
+    const [tab] = await chrome.tabs.query({ currentWindow: true, active: true });
+
+    await chrome.tabs.sendMessage(tab.id, {
+      clearCookies: true,
+    });
+  };
   return (
     <div id="brooklinenPopup" className="bg-[#fdfaf8] text-[#283455] px-5 animate-fade relative">
-      <label className="inline-flex items-center cursor-pointer">
+      <h2 className="text-[12px] font-bold mb-[5px]">On-Page Toggles</h2>
+      <label className="inline-flex items-center cursor-pointer mb-[10px]">
         <input type="checkbox" name="overlayGrid" onClick={e => toggleValue(e)} className="sr-only peer" />
-        <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#283455] dark:peer-checked:bg-blue-600"></div>
-        <span className="ms-3 text-sm font-medium text-[#283455]">
+        <div className="scale-75 relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#283455] dark:peer-checked:bg-blue-600"></div>
+        <span className="ms-3 text-[12px] text-[#283455]">
           Show Overlay Grid {sessionStorage.getItem('overlayGrid')}
         </span>
       </label>
-      <br />
 
-      <label className="inline-flex items-center cursor-pointer">
+      <label className="inline-flex items-center cursor-pointer mb-[10px]">
         <input type="checkbox" name="previewBar" onClick={e => toggleValue(e)} className="sr-only peer" />
-        <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#283455] dark:peer-checked:bg-blue-600"></div>
-        <span className="ms-3 text-sm font-medium text-[#283455] dark:text-gray-300">
+        <div className="scale-75 relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#283455] dark:peer-checked:bg-blue-600"></div>{' '}
+        <span className="ms-3 text-[12px] text-[#283455] dark:text-gray-300">
           Show Preview Bar {sessionStorage.getItem('previewBar')}
         </span>
       </label>
 
-      <br />
-
+      <h2 className="text-[12px] font-bold mb-[5px]">Cookies</h2>
       <form onSubmit={e => handleSubmit(e)}>
-        <label for="cookieInput" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
+        <label for="cookieInput" className="mb-2 text-[10px] text-gray-900 sr-only dark:text-white">
           Add _bl_dev__ cookie
         </label>
-        <div class="relative">
-          <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+        <div className="relative">
+          <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -111,19 +111,28 @@ const Popup = () => {
           <input
             type="cookieInput"
             id="search"
-            class="block w-full p-4 ps-10 pr-[95px] text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="block w-full p-4 ps-10 pr-[95px] text-[12px] text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Cookie Name"
             required
           />
           <button
             type="submit"
-            class="text-white absolute end-2.5 bottom-2.5 bg-[#283455] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            className="text-white absolute end-2.5 bottom-2.5 bg-[#283455] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-[12px] px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Add
           </button>
         </div>
       </form>
 
-      <div className="text-[10px] absolute bottom-[10px] flex">
+      <div className="mt-[10px] mb-[10px]">
+        <button
+          onClick={() => {
+            clearDevCookies();
+          }}
+          className="w-full text-white end-2.5 bottom-2.5 bg-[#283455] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-[12px] px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          Clear All Dev Cookies
+        </button>
+      </div>
+      <div className="text-[10px] absolute bottom-[-20px] flex">
         <button
           className="flex mr-[16px]"
           onClick={() =>
